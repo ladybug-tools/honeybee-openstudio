0413a01dc7c813f04fc1974f41430cd8dab79526
Chris Mackey <chris@ladybug.tools>
2025-03-20 14:30:05 -0700

    fix(ventcool): Add AFN tests and fix bug

--- tests/ventcool_test.py
+++ tests/ventcool_test.py
@@ -1,10 +1,14 @@
 # coding=utf-8
 """Test the translators for ventilative cooling to OpenStudio."""
+import os
+
 from ladybug.dt import Time
 from honeybee.room import Room
+from honeybee.model import Model
 from honeybee_energy.ventcool.control import VentilationControl
 from honeybee_energy.ventcool.opening import VentilationOpening
 from honeybee_energy.ventcool.fan import VentilationFan
+from honeybee_energy.ventcool.afn import generate
 from honeybee_energy.schedule.day import ScheduleDay
 from honeybee_energy.schedule.ruleset import ScheduleRuleset
 import honeybee_energy.lib.scheduletypelimits as schedule_types
@@ -12,6 +16,7 @@ import honeybee_energy.lib.scheduletypelimits as schedule_types
 from honeybee_openstudio.openstudio import OSModel
 from honeybee_openstudio.ventcool import ventilation_opening_to_openstudio, \
     ventilation_fan_to_openstudio
+from honeybee_openstudio.writer import model_to_openstudio
 
 
 def test_ventilation_opening_to_openstudio_simple():
@@ -52,3 +57,36 @@ def test_ventilation_fan_to_openstudio():
     assert str(os_fan.name()) == 'Night Flushing Fan'
     os_fan_str = str(os_fan)
     assert os_fan_str.startswith('OS:ZoneVentilation:DesignFlowRate,')
+
+
+def test_afn_to_openstudio():
+    """Test the translation of a Model with an AFN to OpenStudio."""
+    standard_test = 'assets/afn_apartment.hbjson'
+    standard_test = os.path.join(os.path.dirname(__file__), standard_test)
+    model = Model.from_file(standard_test)
+
+    # generate the AFN leakage for all of the surfaces of the Model
+    generate(model.rooms, leakage_type='Medium', use_room_infiltration=True,
+             atmospheric_pressure=101325, delta_pressure=4)
+
+    # set up the Model-wide VentilationSimulationParameters for the AFN
+    vent_sim_par = model.properties.energy.ventilation_simulation_control
+    vent_sim_par.vent_control_type = 'MultiZoneWithoutDistribution'
+    model.properties.energy.autocalculate_ventilation_simulation_control()
+
+    os_model = model_to_openstudio(model)
+
+    ems_programs = os_model.getEnergyManagementSystemPrograms()
+    assert len(ems_programs) == 3
+    ems_prog_str = str(ems_programs[0])
+    assert ems_prog_str.startswith('OS:EnergyManagementSystem:Program,')
+
+    afn_openings = os_model.getAirflowNetworkSimpleOpenings()
+    assert len(afn_openings) == 5
+    afn_opening_str = str(afn_openings[0])
+    assert afn_opening_str.startswith('OS:AirflowNetworkSimpleOpening,')
+
+    afn_cracks = os_model.getAirflowNetworkCracks()
+    assert len(afn_cracks) == 5
+    afn_crack_str = str(afn_cracks[0])
+    assert afn_crack_str.startswith('OS:AirflowNetworkCrack,')
