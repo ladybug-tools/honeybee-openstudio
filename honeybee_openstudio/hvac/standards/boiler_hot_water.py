2db192dd0f227741b8138afc43b8fb261f9fc1bd
Chris Mackey <chris@ladybug.tools>
2025-03-24 16:47:59 -0700

    fix(hvac): Start adding methods for plant loops

---
+++ honeybee_openstudio/hvac/standards/boiler_hot_water.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+"""Module taken from OpenStudio-standards.
+
+https://github.com/NREL/openstudio-standards/blob/master/
+lib/openstudio-standards/prototypes/common/objects/Prototype.BoilerHotWater.rb
+"""
+from __future__ import division
+
+
+def create_boiler_hot_water(model, hot_water_loop):
+    pass
