6fef92937ac45a400495e7181abf64bb5473202a
Chris Mackey <chris@ladybug.tools>
2025-04-12 08:57:55 -0700

    fix(gbxml): Add an option to use simple window constructions for gbXML

--- honeybee_openstudio/hvac/standards/thermal_zone.py
+++ honeybee_openstudio/hvac/standards/thermal_zone.py
@@ -34,8 +34,7 @@ def thermal_zone_get_outdoor_airflow_rate(thermal_zone):
     for space in spaces:
         floor_area = space.floorArea() if sys.version_info >= (3, 0) else space.floorArea
         number_of_people = space.numberOfPeople()
-        volume = space.volume() if sys.version_info >= (3, 0) else space.volume
-        # volume = 1
+        volume = space.volume()
 
         dsn_oa = space.designSpecificationOutdoorAir()
         if not dsn_oa.is_initialized():
