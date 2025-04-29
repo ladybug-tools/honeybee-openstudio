9013fc158877811d1bb7166a399161c0a6c664b0
Chris Mackey <chris@ladybug.tools>
2025-04-29 14:23:38 -0700

    fix(program): Add a function to translate SpaceType to ProgramType

--- honeybee_openstudio/programtype.py
+++ honeybee_openstudio/programtype.py
@@ -2,9 +2,15 @@
 """OpenStudio ProgramType translator."""
 from __future__ import division
 
+from honeybee.typing import clean_ep_string
+from honeybee_energy.programtype import ProgramType
+
 from honeybee_openstudio.load import people_to_openstudio, lighting_to_openstudio, \
     electric_equipment_to_openstudio, gas_equipment_to_openstudio, \
-    infiltration_to_openstudio, ventilation_to_openstudio
+    infiltration_to_openstudio, ventilation_to_openstudio, people_from_openstudio, \
+    lighting_from_openstudio, electric_equipment_from_openstudio, \
+    gas_equipment_from_openstudio, infiltration_from_openstudio, \
+    ventilation_from_openstudio
 from honeybee_openstudio.openstudio import OSSpaceType
 
 
@@ -55,3 +61,42 @@ def program_type_to_openstudio(program_type, os_model, include_infiltration=True
         os_vent = ventilation_to_openstudio(program_type.ventilation, os_model)
         os_space_type.setDesignSpecificationOutdoorAir(os_vent)
     return os_space_type
+
+
+def program_type_from_openstudio(os_space_type, schedules=None):
+    """Convert OpenStudio SpaceType to Honeybee ProgramType."""
+    program_type = ProgramType(clean_ep_string(os_space_type.nameString()))
+    # assign people
+    for os_people in os_space_type.people():
+        people_def = os_people.peopleDefinition()  # only translate if people per floor
+        if people_def.peopleperSpaceFloorArea().is_initialized():
+            program_type.people = people_from_openstudio(os_people, schedules)
+    # assign lighting
+    for os_lights in os_space_type.lights():
+        light_def = os_lights.lightsDefinition()  # only translate if watts per floor
+        if light_def.wattsperSpaceFloorArea().is_initialized():
+            program_type.lighting = lighting_from_openstudio(os_lights, schedules)
+    # assign electric equipment
+    for os_equip in os_space_type.electricEquipment():
+        electric_eq_def = os_equip.electricEquipmentDefinition()
+        if electric_eq_def.wattsperSpaceFloorArea().is_initialized():
+            program_type.electric_equipment = \
+                electric_equipment_from_openstudio(os_equip, schedules)
+    # assign gas equipment
+    for os_equip in os_space_type.gasEquipment():
+        electric_eq_def = os_equip.gasEquipmentDefinition()
+        if electric_eq_def.wattsperSpaceFloorArea().is_initialized():
+            program_type.gas_equipment = \
+                gas_equipment_from_openstudio(os_equip, schedules)
+    # assign infiltration
+    for os_inf in os_space_type.spaceInfiltrationDesignFlowRates():
+        if os_inf.flowperExteriorSurfaceArea().is_initialized():
+            program_type.infiltration = infiltration_from_openstudio(os_inf, schedules)
+    # assign ventilation
+    if os_space_type.designSpecificationOutdoorAir().is_initialized():
+        os_vent = os_space_type.designSpecificationOutdoorAir().get()
+        program_type.ventilation = ventilation_from_openstudio(os_vent, schedules)
+    # assign the display name and return it
+    if os_space_type.displayName().is_initialized():
+        program_type.display_name = os_space_type.displayName().get()
+    return program_type
--- tests/program_type_test.py
+++ tests/program_type_test.py
@@ -1,14 +1,17 @@
 # coding=utf-8
 """Test the translators for ConstructionSets to OpenStudio."""
 from honeybee_energy.lib.programtypes import office_program
+import honeybee_energy.lib.scheduletypelimits as schedule_types
 
 from honeybee_openstudio.openstudio import OSModel
-from honeybee_openstudio.schedule import schedule_to_openstudio
-from honeybee_openstudio.programtype import program_type_to_openstudio
+from honeybee_openstudio.schedule import schedule_to_openstudio, \
+    schedule_type_limits_to_openstudio, extract_all_schedules
+from honeybee_openstudio.programtype import program_type_to_openstudio, \
+    program_type_from_openstudio
 
 
 def test_program_type_to_openstudio():
-    """Test the translation of People to OpenStudio."""
+    """Test the translation of ProgramType to OpenStudio."""
     os_model = OSModel()
     os_model.setDayofWeekforStartDay('Sunday')  # this avoids lots of warnings
 
@@ -19,3 +22,41 @@ def test_program_type_to_openstudio():
     assert str(os_space_type.name()) == 'Generic Office Program'
     os_space_type_str = str(os_space_type)
     assert os_space_type_str.startswith('OS:SpaceType,')
+
+
+def test_program_type_from_openstudio():
+    """Test the translation of ProgramType from OpenStudio."""
+    os_model = OSModel()
+    os_model.setDayofWeekforStartDay('Sunday')  # this avoids lots of warnings
+    schedule_type_limits_to_openstudio(schedule_types.fractional, os_model)
+    for schedule in office_program.schedules_unique:
+        schedule_to_openstudio(schedule, os_model)
+    os_space_type = program_type_to_openstudio(office_program, os_model)
+
+    schedules = extract_all_schedules(os_model)
+    rebuilt_program = program_type_from_openstudio(os_space_type, schedules)
+
+    ppld = office_program.people.people_per_area
+    assert ppld - 0.001 < rebuilt_program.people.people_per_area < ppld + 0.001
+    assert office_program.people.occupancy_schedule.identifier == \
+        rebuilt_program.people.occupancy_schedule.identifier
+
+    lpd = office_program.lighting.watts_per_area
+    assert lpd - 0.001 < rebuilt_program.lighting.watts_per_area < lpd + 0.001
+    assert office_program.lighting.schedule.identifier == \
+        rebuilt_program.lighting.schedule.identifier
+
+    epd = office_program.electric_equipment.watts_per_area
+    assert epd - 0.001 < rebuilt_program.electric_equipment.watts_per_area < epd + 0.001
+    assert office_program.electric_equipment.schedule.identifier == \
+        rebuilt_program.electric_equipment.schedule.identifier
+
+    fd = office_program.infiltration.flow_per_exterior_area
+    assert fd - 0.001 < rebuilt_program.infiltration.flow_per_exterior_area < fd + 0.001
+    assert office_program.infiltration.schedule.identifier == \
+        rebuilt_program.infiltration.schedule.identifier
+
+    fpp = office_program.ventilation.flow_per_person
+    fpa = office_program.ventilation.flow_per_area
+    assert fpp - 0.00001 < rebuilt_program.ventilation.flow_per_person < fpp + 0.00001
+    assert fpa - 0.00001 < rebuilt_program.ventilation.flow_per_area < fpa + 0.00001
